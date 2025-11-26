4dffdf375fbe157f042f74de920cfcbe2f1c2209
erik <luznoire@gmail.com>
2025-11-26 17:35:48 -0600

    Ver-1

--- app.py
+++ app.py
@@ -9,8 +9,17 @@ hist_button = st.button('Construir histograma') # crear un botón
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
+     
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    ...
