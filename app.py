e6c47d039e662bdfa98c2d265afe85e363f33b66
erik <luznoire@gmail.com>
2025-11-26 16:55:48 -0600

    Ver-5

--- app.py
+++ app.py
@@ -13,4 +13,12 @@ if hist_button: # al hacer clic en el botón
     fig = px.histogram(car_data, x="odometer")
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+# crear gráfico de dispersión --- IGNORE ---
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+fig.show() # crear gráfico de dispersión
      
