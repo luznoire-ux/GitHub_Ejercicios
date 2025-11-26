9cf7a4cefb833136f22bab3aae9905d5d7a01410
erik <luznoire@gmail.com>
2025-11-26 16:12:36 -0600

    Ver-1

--- kk.py
+++ kk.py
@@ -1,1 +1,16 @@
 # Hola KK
+import plotly.express as px
+  
+import streamlit  
+print("Hola KK")
+print("streamlit",streamlit.__version__)
+
+
+try:
+    import nbformat
+    print("La librería nbformat está instalada.")
+    print(f"Versión de nbformat: {nbformat.__version__}")
+except ImportError:
+    print("La librería nbformat NO está instalada.")
+except Exception as e:
+    print(f"Ocurrió un error: {e}")
