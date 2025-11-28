67fd0c0787f9c31e28318ea6ad825b8032d1a4ca
erik <luznoire@gmail.com>
2025-11-28 17:36:30 -0600

    Ver-1

--- app.py
+++ app.py
@@ -23,3 +23,6 @@ build_histogram = st.checkbox('Construir un histograma')
 if build_histogram: # si la casilla de verificación está seleccionada
     st.write('Construir un histograma para la columna odómetro')
     ...
+
+
+#**************
